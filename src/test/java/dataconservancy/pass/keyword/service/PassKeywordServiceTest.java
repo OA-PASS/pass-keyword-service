e91a4e129cb9d1c76a316baeab53ae399bf10779
Jihoon Kim <j.kim25@outlook.com>
2021-11-07 22:36:49 -0500

    added additional structuring

---
+++ src/test/java/dataconservancy/pass/keyword/service/PassKeywordServiceTest.java
@@ -0,0 +1,28 @@
+package org.dataconservancy.pass.doi.service;
+
+import org.dataconservancy.pass.client.PassClient;
+import org.dataconservancy.pass.client.PassJsonAdapter;
+import org.dataconservancy.pass.client.adapter.PassJsonAdapterBasic;
+import org.dataconservancy.pass.model.Journal;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.StringReader;
+import java.net.URI;
+import java.util.*;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+public class PassKeywordServiceTest {
+
+
+}
