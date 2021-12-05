9ecd590612d86fc87df4ea3298ce6cfe71c5afa9
Jihoon Kim <j.kim25@outlook.com>
2021-12-05 03:42:52 -0500

    added Servlet Tests

--- src/test/java/org/dataconservancy/pass/keyword/service/PassKeywordServletTest.java
+++ src/test/java/org/dataconservancy/pass/keyword/service/PassKeywordServletTest.java
@@ -3,7 +3,6 @@ package org.dataconservancy.pass.keyword.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
